0d78cd40c3e2abcfc4c731ab523f4164fb568f55
MrsKing2023 <smarshall84@yahoo.com>
2023-10-26 14:57:47 -0400

    annotations not working

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,7 @@
 package org.launchcode.codingevents.models;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
